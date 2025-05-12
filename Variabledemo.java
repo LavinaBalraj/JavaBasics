29c5a7a2388fc77c9ad782656887493ee0cf51d1
LavinaBalraj <lavinabalraj2020@gmail.com>
2025-05-12 04:28:25 +0200

    Add files via upload

---
+++ Variabledemo.java
@@ -0,0 +1,15 @@
+package Basics;
+
+public class Variabledemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a=100;
+		System.out.println("Value of a is :"+a);
+		int b=300,c=500;
+		System.out.println("Value of a and b and c is\n");
+		System.out.println(a+" "+b+" "+c);
+	}
+
+}
